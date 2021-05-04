25fd0ba265707c5aeaac4c469097afc5abef8f06
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-04 10:06:49 -0300

    Fazendo o binding da tela HTML com o backend

--- backend/cadastroanuncios/src/main/java/com/augustobellinaso/cadastroanuncios/controller/AnuncioController.java
+++ backend/cadastroanuncios/src/main/java/com/augustobellinaso/cadastroanuncios/controller/AnuncioController.java
@@ -1,13 +1,16 @@
 package com.augustobellinaso.cadastroanuncios.controller;
 
+import com.augustobellinaso.cadastroanuncios.domain.anuncio.Anuncio;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AnuncioController {
 
     @GetMapping(path = {"/", "/index"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("anuncio", new Anuncio());
         return "index";
     }
 }
--- backend/cadastroanuncios/src/main/java/com/augustobellinaso/cadastroanuncios/domain/anuncio/Anuncio.java
+++ backend/cadastroanuncios/src/main/java/com/augustobellinaso/cadastroanuncios/domain/anuncio/Anuncio.java
@@ -1,6 +1,8 @@
 package com.augustobellinaso.cadastroanuncios.domain.anuncio;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +14,8 @@ import java.time.LocalDate;
 
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
 public class Anuncio implements Serializable {
 
     @Id
