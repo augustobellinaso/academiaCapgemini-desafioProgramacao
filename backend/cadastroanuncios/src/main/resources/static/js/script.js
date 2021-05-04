b546c8f217820db4c21782474ce208d401fb0948
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-04 10:56:19 -0300

    Validando o campo de investimento para receber somente números

---
+++ backend/cadastroanuncios/src/main/resources/static/js/script.js
@@ -0,0 +1,10 @@
+function isNumberKey(evt)  {
+
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+
+    if (charCode >= 48 && charCode <= 57) {
+        return true;
+    }
+
+    return false;
+}
