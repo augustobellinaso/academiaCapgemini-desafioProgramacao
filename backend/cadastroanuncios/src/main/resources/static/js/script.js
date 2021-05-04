c10b6e56a47f7045ff401f1054a1f11e2bd74ce5
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-04 11:05:45 -0300

    Modificando a função para validar campo numérico

--- backend/cadastroanuncios/src/main/resources/static/js/script.js
+++ backend/cadastroanuncios/src/main/resources/static/js/script.js
@@ -2,7 +2,7 @@ function isNumberKey(evt)  {
 
     var charCode = (evt.which) ? evt.which : evt.keyCode;
 
-    if (charCode >= 48 && charCode <= 57) {
+    if ((charCode >= 48 && charCode <= 57) || charCode <= 31) {
         return true;
     }
 
