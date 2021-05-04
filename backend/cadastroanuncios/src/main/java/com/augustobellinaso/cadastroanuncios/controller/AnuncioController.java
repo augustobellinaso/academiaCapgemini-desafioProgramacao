e7a3beea074c629c36424e572c860c048d50ad56
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-04 09:51:00 -0300

    Criando o Controller

---
+++ backend/cadastroanuncios/src/main/java/com/augustobellinaso/cadastroanuncios/controller/AnuncioController.java
@@ -0,0 +1,13 @@
+package com.augustobellinaso.cadastroanuncios.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AnuncioController {
+
+    @GetMapping(path = {"/", "/index"})
+    public String index() {
+        return "index";
+    }
+}
