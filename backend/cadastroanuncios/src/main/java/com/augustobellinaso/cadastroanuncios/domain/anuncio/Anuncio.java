b96757ca57a85c0ac3498d158da4b9ade2c694eb
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-04 13:36:47 -0300

    Alterando o tipo de dado das datas para LocalDate

--- backend/cadastroanuncios/src/main/java/com/augustobellinaso/cadastroanuncios/domain/anuncio/Anuncio.java
+++ backend/cadastroanuncios/src/main/java/com/augustobellinaso/cadastroanuncios/domain/anuncio/Anuncio.java
@@ -3,11 +3,9 @@ package com.augustobellinaso.cadastroanuncios.domain.anuncio;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -24,21 +22,29 @@ public class Anuncio implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
+    @Column
     private Integer id;
 
     @NotBlank(message = "O nome do anúncio deve ser preenchido")
+    @Column
     private String nomeAnuncio;
 
     @NotBlank(message = "O nome do cliente deve ser preenchido")
+    @Column
     private String nomeCliente;
 
-    @NotBlank(message = "A data de início do anúncio deve ser preenchida")
-    private String dataInicio;
+    @NotNull(message = "A data de início do anúncio deve ser preenchida")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column
+    private LocalDate dataInicio;
 
-    @NotBlank(message = "A data de término do anúncio deve ser preenchida")
-    private String dataFinal;
+    @NotNull(message = "A data de término do anúncio deve ser preenchida")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column
+    private LocalDate dataFinal;
 
     @NotNull(message = "O valor do investimento deve ser preenchido")
+    @Column
     private BigDecimal investimento;
 
 }
